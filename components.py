b1f5ed0a80714d82536cdfbcd1f0ebe97ca5ca87
markushoehn2007 <markushoehn2007@gmail.com>
2023-10-14 15:36:15 -0500

    pid control

--- components.py
+++ components.py
@@ -42,6 +42,8 @@ class DriveBase:
 
 class Arm:
     def __init__(self, controller):
+        self.mode = "IDLE"
+
         # Intializes motors for the arm.
         self.shoulderMotor = rev.CANSparkMax(CAN.shoulderChannel, rev.CANSparkMax.MotorType.kBrushless)
         self.extenderMotor = rev.CANSparkMax(CAN.extenderChannel, rev.CANSparkMax.MotorType.kBrushless)
@@ -51,7 +53,17 @@ class Arm:
         self.extenderMotor.restoreFactoryDefaults()
         self.intakeMotor.restoreFactoryDefaults()
 
+        # Sets up shoulder PID controller.
+        self.shoulderPIDController = self.shoulderMotor.getPIDController()
+        self.shoulderPIDController.setP(0.1)
+        self.shoulderPIDController.setI(0.01)
+        self.shoulderPIDController.setD(0.001)
+        self.shoulderEncoder = self.shoulderMotor.getEncoder()
+
         self.controller = controller
+
+    def setMode(self, mode):
+        self.mode = mode
 
     def autonomousInit(self):
         pass
@@ -71,11 +83,20 @@ class Arm:
         else:
             self.intakeMotor.set(0)
 
-        # Handles contorl on the shoulder motor.
+        # Handles control on the shoulder motor.
         if self.controller.getYButton():
-            self.shoulderMotor.set(0.26)
+            self.shoulderMotor.set(0.25)
         elif self.controller.getXButton():
             self.shoulderMotor.set(-0.25)
         else:
-            self.shoulderMotor.set(0)
+            targetPosition = self.shoulderEncoder.getPosition()
+            self.shoulderPIDController.setReference(targetPosition, rev.CANSparkMax.ControlType.kPosition)
+
+        # Handles control on the extender motor.
+        if self.controller.getPOV() == 0:
+            self.extenderMotor.set(0.25)
+        elif self.controller.getPOV() == 90:
+            self.extenderMotor.set(-0.25)
+        else:
+            self.extenderMotor.set(0)
             
